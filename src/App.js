f0284adfec3dd5f97ab68a4bee3e1915d17fdc4e
amyeguo <72362221+amyeguo@users.noreply.github.com>
2023-05-24 22:01:59 -0500

    moved components into src folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from "./logo.svg";
+import CreateCard from "./Components/CreateCard";
+
 import "./App.css";
 
 function App() {
@@ -8,14 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CreateCard />
       </header>
     </div>
   );
