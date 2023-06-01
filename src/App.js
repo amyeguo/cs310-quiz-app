bb9dfe37a4af671cbed68a64e159be26d92de23e
Lam-Richard <richardlam2024@gmail.com>
2023-05-31 21:53:04 -0500

    basic material ui frontend done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,96 @@
-import logo from "./logo.svg";
-import CreateCard from "./Components/CreateCard";
+// import CreateCard from "./components/CreateCard";
+import { Card, CardContent, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+
 
 import "./App.css";
 
+import React, { useState } from 'react';
+
+
+const Flashcard = ({ question }) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  function toggleShowAnswer() {
+    setShowAnswer(!showAnswer)
+  }
+  return (
+    showAnswer ? 
+    
+    <Card sx={{width: '20rem'}}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          Answer
+        </Typography>
+        <br></br>
+        
+        <Typography variant="body2" color="text.secondary">
+          {question.answer}
+        </Typography>
+        <br></br>
+
+        <Button variant="contained" color="primary"  onClick={() => toggleShowAnswer()}>
+          Show Question
+        </Button>
+        <br></br>
+      </CardContent>
+    </Card> 
+
+    :
+
+    <Card sx={{width: '20rem'}}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          Question
+        </Typography>
+        <br></br>
+        
+        <Typography variant="body2" color="text.secondary">
+          {question.question}
+        </Typography>
+        <br></br>
+
+        <Button variant="contained" color="primary" onClick={() => toggleShowAnswer()}>
+          Show Answer
+        </Button>
+        <br></br>
+      </CardContent>
+    </Card>  
+  )
+}
+
+
 function App() {
+  
+
+  const [question, setQuestion] = useState({question: "What is the powerhouse of the cell?", answer: "mitochondria"})
+
+  const questions = [
+    {
+      question: "What organelle lives in the rough ER?",
+      answer: "ribosome",
+    },
+    {
+      question: "What is the Big O runtime of merge sort?",
+      answer: "nlogn"
+    }, 
+    {
+      question: "What does GPU stand for?",
+      answer: "graphical processing unit"
+    }
+  ]
+
+  function getNextQuestion() {
+    setQuestion(questions[Math.floor(Math.random() * questions.length)]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <CreateCard />
-      </header>
+      <Flashcard key={crypto.randomUUID()} question={question}></Flashcard>
+      <br></br>
+      <Button variant="contained" color="primary" onClick={() => getNextQuestion()}>
+          Show Next Question
+      </Button>
     </div>
   );
 }
