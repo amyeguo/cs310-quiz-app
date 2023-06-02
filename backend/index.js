c7659f1e4bd34b489a6ceefa4369989e6c408017
amyeguo <72362221+amyeguo@users.noreply.github.com>
2023-06-01 23:36:28 -0500

    Added random list of questions

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-const dbConnection = require('./database.js')
+const dbConnection = require("./database.js");
 
-app.use(cors({
-  origin: 'http://localhost:3001'
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
 
 // Define routes
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-  console.log("Are you printing this instead?");
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 app.get("/test", (req, res) => {
-    try {
-        var sql = "SELECT * FROM questions ORDER BY RAND() LIMIT 1;";
-        console.log("Do you even get here...");
+  try {
+    var sql = "SELECT * FROM questions ORDER BY RAND()";
 
-        dbConnection.query(sql, (err, rows) => {
-          //
-          // query has finished:
-          //
-          console.log(err);
-          console.log(rows);
-        
-        res.status(200).json({
-            "message": rows
-        });
+    dbConnection.query(sql, (err, rows) => {
+      //
+      // query has finished:
+      //
+      console.log(err);
+      console.log(rows);
+
+      res.status(200).json({
+        message: rows,
+      });
     });
-    }  catch (err) {
-        res.status(400).json({
-          "message": err.message
-        });
-    }
-})
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,123 +1,46 @@
 // import CreateCard from "./components/CreateCard";
 import { Card, CardContent, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
-
 import "./App.css";
-
 import React, { useState, useEffect } from "react";
-
-const Flashcard = ({ question }) => {
-  const [showAnswer, setShowAnswer] = useState(false);
-
-  function toggleShowAnswer() {
-    setShowAnswer(!showAnswer);
-  }
-
-  const cardStyle = {
-    width: "40rem",
-    height: "20rem",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    border: "2px solid #0078D7",
-    boxShadow: "none",
-  };
-
-  const cardContentStyle = {
-    margin: "auto",
-    textAlign: "center",
-    color: "#0078D7",
-  };
-
-  return showAnswer ? (
-    <Card style={cardStyle}>
-      <CardContent style={cardContentStyle} sx={{ width: "80%" }}>
-        <Typography variant="h5" component="div" sx={{ fontWeight: "bold" }}>
-          Answer
-        </Typography>
-        <br></br>
-
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          sx={{ fontSize: "30px" }}
-        >
-          {question.answer_text}
-        </Typography>
-        <br></br>
-
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ textTransform: "none" }}
-          onClick={() => toggleShowAnswer()}
-        >
-          Show Question
-        </Button>
-        <br></br>
-      </CardContent>
-    </Card>
-  ) : (
-    <Card style={cardStyle}>
-      <CardContent style={cardContentStyle} sx={{ width: "80%" }}>
-        <Typography variant="h5" component="div" sx={{ fontWeight: "bold" }}>
-          Question
-        </Typography>
-        <br></br>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          sx={{ fontSize: "30px" }}
-        >
-          {question.question_id-2000}.{question.question_text}
-        </Typography>
-        <br></br>
-
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ textTransform: "none" }}
-          onClick={() => toggleShowAnswer()}
-        >
-          Show Answer
-        </Button>
-        <br></br>
-      </CardContent>
-    </Card>
-  );
-};
-
-
+import Flashcard from "./components/Flashcard";
 
 function App() {
+  const [questionIndex, setQuestionIndex] = useState(null);
+  const [questionList, setQuestionList] = useState([]);
+  const [question, setQuestion] = useState({});
 
-const [question, setQuestion] = useState( {question: "What is the powerhouse of the cell?", answer: "mitochondria"}    )
-
-
-function getNextQuestion() {
-
-    console.log("Called?")
-
+  function getQuestionList() {
     fetch("http://localhost:3000/test")
-    .then((response) => response.json())
-    .then((data) => { 
-      console.log(data);
-      setQuestion(data['message'][0]); 
-    })
-    
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestionList(data["message"]);
+      });
+  }
+
+  function getNextQuestion() {
+    let currInd = questionIndex;
+    if (currInd < questionList.length) {
+      let newInd = currInd + 1;
+      setQuestionIndex(newInd);
+      setQuestion(questionList[questionIndex]);
+    }
+  }
+
+  function startQuiz() {
+    setQuestionIndex(0);
+    setQuestion(questionList[0]);
   }
 
   useEffect(() => {
-    getNextQuestion();
-  }, [])
+    getQuestionList();
+  }, []);
 
-  
   const centerStyle = {
     display: "grid",
     placeItems: "center",
     height: "100vh",
   };
-
 
   return (
     <div className="App" style={centerStyle}>
@@ -134,15 +57,28 @@ function getNextQuestion() {
       >
         Quiz App
       </Typography>
-      <Flashcard key={crypto.randomUUID()} question={question}></Flashcard>
-      <Button
-        variant="contained"
-        color="primary"
-        sx={{ textTransform: "none" }}
-        onClick={() => getNextQuestion()}
-      >
-        Next Question
-      </Button>
+      {questionIndex === null ? (
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ textTransform: "none" }}
+          onClick={() => startQuiz()}
+        >
+          Start Quiz
+        </Button>
+      ) : (
+        <>
+          <Flashcard key={crypto.randomUUID()} question={question}></Flashcard>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ textTransform: "none" }}
+            onClick={() => getNextQuestion()}
+          >
+            Next Question
+          </Button>
+        </>
+      )}
     </div>
   );
 }
